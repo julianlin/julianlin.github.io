bb9be84ff1bd0411fc62104478dc34de1e3b4358
julianlin <julianlin510@gmail.com>
2024-04-10 18:52:51 -0700

    convert navigation page from styled-components to emotion

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,78 +1,12 @@
 import { useState }from 'react';
-import styled from 'styled-components';
 
 import {ReactComponent as CloseIcon} from '../../assets/close-icon.svg';
 import {ReactComponent as MenuIcon} from '../../assets/menu-icon.svg';
 
-import { MobileNav } from './styles';
-
-type MobileNavItemsType = {
-  display: boolean;
-};
-
-const MobileNavItems = styled.ul<MobileNavItemsType>`
-  color: ${p => p.theme.colors.primary};
-  display: ${p => p.display ? 'block' : 'none'};
-  font-size: 1.5rem;
-  padding-left: ${p => p.theme.spacing[1]};
-
-  li {
-    cursor: pointer;
-    font-weight: bold;
-    height: ${p => p.theme.spacing[3]};
-    list-style-type: none;
-    padding-bottom: ${p => p.theme.spacing[2]};
-
-    &:hover {
-      color: ${p => p.theme.colors.dark};
-    }
-  }
-`;
-
-const NavItem = styled.a`
-  background: none;
-  border: none;
-  color: ${p => p.theme.colors.primary};
-  cursor: pointer;
-  fill: none;
-  font-size: 1.25rem;
-  font-weight: bold;
-  margin-left: ${p => p.theme.spacing[2]};
-  margin-right: ${p => p.theme.spacing[2]};
-  text-decoration: none;
-
-  &:hover {
-    border-bottom: 2px solid ${p => p.theme.colors.dark};
-    color: ${p => p.theme.colors.dark};
-  }
-
-  @media only screen and (max-width: ${p => p.theme.breakpoints.small}) {
-    display: none;
-  }
-`;
-
-const NavItems = styled.div`
-  margin-left: auto;
-  margin-right: ${p => p.theme.spacing[3]};
-`;
-
-const NavigationContainer = styled.nav`
-  background-color: ${p => p.theme.colors.contrast};
-  display: flex;
-  min-height: 2rem;
-  padding: 1rem;
-  top: 0px;
-  width: 100%;
-
-  svg {
-    margin-left: auto;
-    padding-right: ${p => p.theme.spacing[3]};
-  }
-`;
-
+import { MobileNav, MobileNavItems, NavItem, NavItems, NavigationContainer } from './styles';
 
 const Navigation = (props: any) => {
-  const { pageRef, profileRef, experienceRef, linksRef } = props;
+  const { profileRef, experienceRef, linksRef } = props;
   const [menuOpen, setMenuOpen] = useState(false);
 
   const onClickNavItem = (ref: any) => {
--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -25,3 +25,66 @@ export const MobileNav = styled.div`
     display: none;
   }
 `;
+type MobileNavItemsType = {
+  display: boolean;
+};
+
+export const MobileNavItems = styled.ul<MobileNavItemsType>`
+  color: ${theme.colors.primary};
+  display: ${p => p.display ? 'block' : 'none'};
+  font-size: 1.5rem;
+  padding-left: ${theme.spacing[1]};
+
+  li {
+    cursor: pointer;
+    font-weight: bold;
+    height: ${theme.spacing[3]};
+    list-style-type: none;
+    padding-bottom: ${theme.spacing[2]};
+
+    &:hover {
+      color: ${theme.colors.dark};
+    }
+  }
+`;
+
+export const NavItem = styled.a`
+  background: none;
+  border: none;
+  color: ${theme.colors.primary};
+  cursor: pointer;
+  fill: none;
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-left: ${theme.spacing[2]};
+  margin-right: ${theme.spacing[2]};
+  text-decoration: none;
+
+  &:hover {
+    border-bottom: 2px solid ${theme.colors.dark};
+    color: ${theme.colors.dark};
+  }
+
+  @media only screen and (max-width: ${breakpoints.small}) {
+    display: none;
+  }
+`;
+
+export const NavItems = styled.div`
+  margin-left: auto;
+  margin-right: ${theme.spacing[3]};
+`;
+
+export const NavigationContainer = styled.nav`
+  background-color: ${theme.colors.contrast};
+  display: flex;
+  min-height: 2rem;
+  padding: 1rem;
+  top: 0px;
+  width: 100%;
+
+  svg {
+    margin-left: auto;
+    padding-right: ${theme.spacing[3]};
+  }
+`;
