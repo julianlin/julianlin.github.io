4eb650afa92546f47e8193bd9a95f9effe101e0f
julianlin <julianlin510@gmail.com>
2024-04-22 18:58:53 -0700

    Updates

--- src/components/ExperienceSection/types.ts
+++
@@ -1,4 +0,0 @@
-export type Position = {
-  name: string;
-  description: string[];
-};
--- src/react-app-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react-scripts" />
-declare module '*.mp4' {
-  const src: string;
-  const type: string;
-  export default src;
-}
